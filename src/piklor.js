1256fec5d56844225eb032f06a51a692907946e6
Ionică Bizău <bizauionica@yahoo.com>
2015-05-27 12:15:52 +0300

    JSDoc comments

--- src/piklor.js
+++ src/piklor.js
@@ -1,5 +1,24 @@
 (function (root) {
 
+    /**
+     * Piklor
+     * Creates a new `Piklor` instance.
+     *
+     * @name Piklor
+     * @function
+     * @param {String|Element} sel The element where the color picker will live.
+     * @param {Array} colors An array of strings representing colors.
+     * @param {Object} options An object containing the following fields:
+     *
+     *  - `open` (String|Element): The HTML element or query selector which will open the picker.
+     *  - `openEvent` (String): The open event (default: `"click"`).
+     *  - `style` (Object): Some style options:
+     *    - `display` (String): The display value when the picker is opened (default: `"block"`).
+     *  - `template` (String): The color item template. The `{color}` snippet will be replaced
+     *    with the color value (default: `"<div data-col=\"{color}\" style=\"background-color: {color}\"></div>"`).
+     *
+     * @return {Piklor} The `Piklor` instance.
+     */
     function Piklor(sel, colors, options) {
         var self = this;
         options = options || {};
@@ -13,20 +32,34 @@
         self.colors = colors;
         self.options = options;
         self.render();
+
+        // Handle the open element and event.
         if (options.open) {
             options.open.addEventListener(options.openEvent, function (ev) {
                 self.open();
             });
         }
+
+        // Click on colors
         self.elm.addEventListener("click", function (ev) {
             var col = ev.target.getAttribute("data-col");
             if (!col) { return; }
             self.set(col);
             self.close();
         });
+
         self.close();
     }
 
+    /**
+     * getElm
+     * Finds the HTML element.
+     *
+     * @name getElm
+     * @function
+     * @param {String|Element} el The HTML element or query selector.
+     * @return {HTMLElement} The selected HTML element.
+     */
     Piklor.prototype.getElm = function (el) {
         if (typeof el === "string") {
             return document.querySelector(el);
@@ -34,6 +67,13 @@
         return el;
     };
 
+    /**
+     * render
+     * Renders the colors.
+     *
+     * @name render
+     * @function
+     */
     Piklor.prototype.render = function () {
         var self = this
           , html = ""
@@ -46,21 +86,53 @@
         self.elm.innerHTML = html;
     };
 
+    /**
+     * close
+     * Closes the color picker.
+     *
+     * @name close
+     * @function
+     */
     Piklor.prototype.close = function () {
         this.elm.style.display = "none";
     };
 
+    /**
+     * open
+     * Opens the color picker.
+     *
+     * @name open
+     * @function
+     */
     Piklor.prototype.open = function () {
         this.elm.style.display = this.options.style.display;
     };
 
+    /**
+     * colorChosen
+     * Adds a new callback in the colorChosen callback buffer.
+     *
+     * @name colorChosen
+     * @function
+     * @param {Function} cb The callback function called with the selected color.
+     */
     Piklor.prototype.colorChosen = function (cb) {
         this.cbs.push(cb);
     };
 
-    Piklor.prototype.set = function (c) {
+    /**
+     * set
+     * Sets the color picker color.
+     *
+     * @name set
+     * @function
+     * @param {String} c The color to set.
+     * @param {Boolean} p If `false`, the `colorChosen` callbacks will not be called.
+     */
+    Piklor.prototype.set = function (c, p) {
         var self = this;
         self.color = c;
+        if (p === false) { return; }
         self.cbs.forEach(function (cb) {
             cb.call(self, c);
         });
